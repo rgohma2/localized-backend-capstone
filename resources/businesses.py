99957b43014e548a1b3880a73e0505837acdf8a0
Randy Gohmann <rgohma2@gmail.com>
2020-02-24 16:09:06 -0600

    can successfully delete a business

--- resources/businesses.py
+++ resources/businesses.py
@@ -1,7 +1,7 @@
 import models
 from flask import Blueprint, request, jsonify
 from playhouse.shortcuts import model_to_dict
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 
 # nice to add: if users business is the same address as their house 
@@ -15,7 +15,9 @@ businesses = Blueprint('businesses', 'businesses')
 @businesses.route('/', methods=['GET'])
 def test():
 	businesses = models.Business.select()
+	# creates a list of dictionaries from all the business models
 	business_dicts = [model_to_dict(business) for business in businesses]
+	# pops each business owners address and password
 	[(business['owner'].pop('address'), business['owner'].pop('password')) for business in business_dicts]
 
 	return jsonify(
@@ -25,6 +27,7 @@ def test():
 		), 200
 
 @businesses.route('/', methods=['POST'])
+@login_required
 def create_business():
 	payload = request.get_json()
 
@@ -48,6 +51,7 @@ def create_business():
 		)
 	business_dict = model_to_dict(business)
 	print(business_dict)
+	# pops each business owners address and password
 	business_dict['owner'].pop('address')
 	business_dict['owner'].pop('password')
 	print(business_dict)
@@ -57,6 +61,29 @@ def create_business():
 			message=f'{business.name} successfully created!',
 			status=200
 		), 200
+
+@businesses.route('/<id>', methods=['Delete'])
+@login_required
+def delete_business(id):
+	business = models.Business.get_by_id(id)
+	name = business.name
+	if current_user.id == business.owner.id:
+		business.delete_instance()
+		return jsonify(
+				data={},
+				message=f'Your business {name} has been deleted!',
+				status=200
+			), 200
+	else:
+		return jsonify(
+				data={},
+				message=f'You do not own {name}',
+				status=401
+			), 401
+
+
+
+
 
 
 
