0a91be4076ebc6ee797f0cd8cf2aabefc1d684f5
Randy Gohmann <rgohma2@gmail.com>
2020-02-26 12:35:13 -0600

    refactored subscription index and created delete sub route

--- resources/subscriptions.py
+++ resources/subscriptions.py
@@ -15,18 +15,28 @@ def subscription_index():
 	# gets the businesses being followed from the subscriptions
 	businesses_being_followed = [subscription.following for subscription in subscriptions_query]
 
-	post_dicts=[]
-	for i in range (0, len(businesses_being_followed)):
-		posts = businesses_being_followed[i].posts
-		print(posts)
-		# post_dicts=[model_to_dict(post) for post in posts]
-		for j in range (0, len(posts)):
-			print("THIS IS THE POST DICT")
-			post = model_to_dict(posts[j])
-			post['business'].pop('owner')
-			post['business'].pop('address')
-			post_dicts.append(post)
-	print(post_dicts)
+	# post_dicts=[]
+	# for i in range (0, len(businesses_being_followed)):
+	# 	posts = businesses_being_followed[i].posts
+	# 	print(posts)
+	# 	# looping through posts of each business and cleaning up data before appending it
+	# 	for j in range (0, len(posts)):
+	# 		print("THIS IS THE POST DICT")
+	# 		post = model_to_dict(posts[j])
+	# 		post['business'].pop('owner')
+	# 		post['business'].pop('address')
+	# 		post_dicts.append(post)
+	# print(post_dicts)
+
+	# finding posts where follower id matches logged in user id
+	posts = (models.Post
+		.select()
+		.join(models.Business)
+		.join(models.Subscription)
+		.where(models.Subscription.follower == current_user.id))
+	post_dicts = [model_to_dict(post) for post in posts]
+	[(post['business'].pop('owner'), post['business'].pop('address')) for post in post_dicts]
+
 
 
 	return jsonify(
@@ -56,6 +66,46 @@ def subscribe(id):
 				message=f'you are already subscribed to {business.name}',
 				status=401
 			), 401
+
+@subscriptions.route('/<business_id>', methods=['Delete'])
+@login_required
+def delete_sub(business_id):
+	try:
+
+		subscription = (
+			models.Subscription
+			.get(models.Subscription.follower == current_user.id
+			, models.Subscription.following == business_id))
+
+
+		print('SUBSCRIPTION TO DELETE')
+		print(model_to_dict(subscription))
+
+		subscription.delete_instance()
+
+		return jsonify(
+				data={},
+				message='Unsubscribed!',
+				status=200
+			), 200
+	except:
+		return jsonify(
+				data={},
+				message='You are trying to remove a subscription you dont have.',
+				status=401
+			), 401
+
+
+
+	
+
+
+
+
+
+
+
+
 
 
 
